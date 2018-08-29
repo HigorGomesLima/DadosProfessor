29a5be14858c2f22eff449c6b4838570d877facd
puc <puc@lab-ecec>
2018-08-29 11:57:08 -0300

    1

---
+++ src/DadosProfessor/DadosProfessor.java
@@ -0,0 +1,11 @@
+package DadosProfessor;
+
+
+public class DadosProfessor {
+
+    public static void main(String[] args) {
+        PersistenciaLista list = new PersistenciaLista();
+        
+    }
+    
+}
---
+++ src/DadosProfessor/PersistenciaLista.java
@@ -0,0 +1,74 @@
+package DadosProfessor;
+
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+
+
+public class PersistenciaLista {
+    
+    List<Professor> listaDados;
+
+    public PersistenciaLista() {
+
+        try {
+            //Leitura arquivo Professores.csv 
+            FileReader arq = new FileReader("Professores.csv");
+            BufferedReader lerArq = new BufferedReader(arq);
+            String linha = lerArq.readLine();
+            List listaProf = new ArrayList<String>();
+            while (linha != null) {
+                listaProf.add(linha);
+                linha = lerArq.readLine();
+            }
+            arq.close();
+
+            //Leitura arquivo DadosGerais.csv 
+            arq = new FileReader("DadosGerais.csv");
+            lerArq = new BufferedReader(arq);
+            linha = lerArq.readLine();
+            List listaDet = new ArrayList<String>();
+            while (linha != null) {
+                listaDet.add(linha);
+                linha = lerArq.readLine();
+            }
+            
+            arq.close();
+
+            listaDados = new ArrayList<Professor>();
+            Iterator<String> itListaProf = listaProf.iterator();
+            Iterator<String> itListaDet = listaDet.iterator();
+            
+            while(itListaProf.hasNext()){
+                String aux = itListaProf.next();
+                String[] auxS = aux.split(";");
+                System.out.println(auxS[0]);
+                while(itListaDet.hasNext()){
+                    String auxDet = itListaDet.next();
+                    String[] auxDetS = auxDet.split(";");
+                    if(auxS[0].equals(auxDetS[0])){
+                        Professor atual = new Professor();
+                        atual.setMatricula(auxS[0]);
+                        atual.setNome(auxS[1]);
+                        atual.setDepartamento(auxDetS[1]);
+                        atual.setTitulacao(auxDetS[2]);
+                        atual.setHorista(auxDetS[3]);
+                        listaDados.add(atual);
+                        System.out.println(atual.getNome());
+                    }
+                }
+            }
+            
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Não foi possivel abrir o arquivo");
+        }
+    }
+    
+    public Iterator getInformacao(){
+        Iterator retorno = listaDados.iterator();
+        return retorno;
+    }
+
+}
